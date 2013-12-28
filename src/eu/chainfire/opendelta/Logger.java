8810b64d1d6adad3eb601339561b0256bf8ae7b4
Jorrit Jongma <git@jongma.org>
2013-12-28 11:43:41 +0100

    Force debug logger to English locale formatting

--- src/eu/chainfire/opendelta/Logger.java
+++ src/eu/chainfire/opendelta/Logger.java
@@ -1,6 +1,8 @@
 
 package eu.chainfire.opendelta;
 
+import java.util.Locale;
+
 public class Logger {
     private final static String LOG_TAG = "OpenDelta";
     
@@ -12,7 +14,7 @@ public class Logger {
 
     public static void d(String message, Object... args) {
         if (log)
-            android.util.Log.d(LOG_TAG, String.format(message, args));
+            android.util.Log.d(LOG_TAG, String.format(Locale.ENGLISH, message, args));
     }
 
     public static void ex(Exception e) {
@@ -21,6 +23,6 @@ public class Logger {
     }
 
     public static void i(String message, Object... args) {
-        android.util.Log.i(LOG_TAG, String.format(message, args));
+        android.util.Log.i(LOG_TAG, String.format(Locale.ENGLISH, message, args));
     }
 }
