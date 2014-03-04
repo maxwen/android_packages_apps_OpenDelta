1880bb6dea890986fa30d386f6c43aceb229a80c
ByteHamster <info@bytehamster.com>
2014-03-04 10:12:56 +0100

    OpenDelta: Add launcher shortcut
    The requested shortcut in Settings is not possible
    so let's do it this way.
    http://jira.omnirom.org/browse/CONCEPT-15
    
    Change-Id: I07cbe8e194795b074c24dbf1d85d829c5b967420

---
+++ src/eu/chainfire/opendelta/Shortcut.java
@@ -0,0 +1,22 @@
+package eu.chainfire.opendelta;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class Shortcut extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(this, MainActivity.class));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, this.getResources().getString(R.string.title));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher_settings));
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+}
