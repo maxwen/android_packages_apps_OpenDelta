be0976a223060377b78e13da22d0d8226dd8c5be
XpLoDWilD <me@xplod.fr>
2014-11-13 19:39:54 +0000

    Bump OpenDelta style to Material
    Change-Id: I37ca0156714775f87bc8b72215c04712c7f39991

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_SDK_VERSION := 19
+LOCAL_SDK_VERSION := 21
 
 LOCAL_JNI_SHARED_LIBRARIES := libopendelta
 LOCAL_REQUIRED_MODULES := libopendelta
